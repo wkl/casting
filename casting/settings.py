c83bb3684cb142cd00bb2a99d112bd9379884ccb
Conan Wang <buaawkl@gmail.com>
2013-07-30 03:23:05 +0800

    initial scrapy(0.16) skeleton

---
+++ casting/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for casting project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'casting'
+
+SPIDER_MODULES = ['casting.spiders']
+NEWSPIDER_MODULE = 'casting.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'casting (+http://www.yourdomain.com)'
